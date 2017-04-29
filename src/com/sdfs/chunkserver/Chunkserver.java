11734b7c876021befacfa2bf62feb6328526ea66
mmbohari <iib8085@rit.edu>
2017-04-29 01:16:02 -0400

    Established communication between master server and chunk server.
    Implemented functionality in
    MasterServer
    1) Listens for requests
    2) Upon reception of registration request from chunk server, assign it a
    role of chunkserver or backup chunkserver
    
    ChunkServer
    1)Sends registration request upon initialization

--- src/com/sdfs/chunkserver/Chunkserver.java
+++ src/com/sdfs/chunkserver/Chunkserver.java
@@ -1,5 +1,11 @@
 package com.sdfs.chunkserver;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 import java.util.Scanner;
 
 /**
@@ -10,9 +16,14 @@ import java.util.Scanner;
  */
 public class Chunkserver {
 
-	static int listeningPort;
-	static String chunkServerName;
+	private static int listeningPort;
+	private static int listeningPortMasterServer;
+	private static String chunkServerName;
 	Scanner sc;
+	
+	Socket socket;
+	DataOutputStream dout;
+	DataInputStream din;
 	
 	/*
 	 * Constructor of Chunkserver
@@ -22,20 +33,34 @@ public class Chunkserver {
 	}
 	
 	/*
-	 * This method is used to take port number that the Chunkserver
-	 * listens for connections
+	 * This method is used to initialize chunkservers
 	 */
-	public int inputListeningPort(){
-		sc = new Scanner(System.in);
+	public void initialise(){
+		displayLines();
+		System.out.println("Initialising chunkserver");
 		
-		System.out.println("Enter listening port for " + chunkServerName);
-		try {
-			listeningPort = sc.nextInt();
-		} catch (Exception e) {
-			System.err.println("Integer value expected for listening"
-					+ " port (>1024 && 64k<)");
-		}
-		return listeningPort;
+		// input chunkserver number
+		chunkServerName += inputChunkserverNumber();
+		// input listening port
+		do {
+			listeningPort = inputListeningPort();
+		} while (listeningPort == 0);
+		System.out.println( chunkServerName + " will listen for connections"
+				+ " on port: " + listeningPort);
+		displayCross();
+		
+		// input master server's listening port
+		do {
+			listeningPortMasterServer = inputListeningPortMasterServer();
+		} while (listeningPortMasterServer == 0);
+		System.out.println( "Master Server listens for requests"			// remove
+				+ " on port: " + listeningPortMasterServer);				
+		
+		// register with master server
+		System.out.println("Registering with master server..");
+		registerMasterServer();
+		
+		displayLines();
 	}
 	
 	/*
@@ -58,28 +83,118 @@ public class Chunkserver {
 	}
 	
 	/*
-	 * This method is used to initialize chunkservers
+	 * This method is used to take port number that the Chunkserver
+	 * listens for connections
 	 */
-	public void initialise(){
-		displayLines();
-		System.out.println("Initialising chunkserver");
-		// input chunkserver number
-		chunkServerName += inputChunkserverNumber();
-		// input listening port
-		do {
-			listeningPort = inputListeningPort();
-		} while (listeningPort == 0);
-		System.out.println( chunkServerName + " will listen for connections"
-				+ " on port: " + listeningPort);
-		displayLines();
+	public int inputListeningPort(){
+		sc = new Scanner(System.in);
+		int port = 0;
+		
+		System.out.println("Enter listening port for " + chunkServerName);
+		try {
+			port = sc.nextInt();
+		} catch (Exception e) {
+			System.err.println("Integer value expected for listening"
+					+ " port (>1024 && 64k<)");
+		}
+		return port;
 	}
 	
+	/*
+	 * This method is used to take listening port of master server
+	 * that the master server uses to listen for incoming requests
+	 */
+	public int inputListeningPortMasterServer(){
+		sc = new Scanner(System.in);
+		int port = 0;
+		
+		System.out.println("Enter listening port that the Master Server"
+				+ " is using to listen for incoming requests");
+		try {
+			port = sc.nextInt();
+		} catch (Exception e) {
+			System.err.println("Integer value expected for listening"
+					+ " port master server(>1024 && 64k<)");
+		}
+		return port;
+	}
+	
+	/*
+	 * This method is used to register chunkserver.
+	 * Chunkserver sends a request to master server. The master server
+	 * register's the respective chunkserver upon reception of request
+	 * as either chunkserver or backup chunkserver.
+	 */
+	public void registerMasterServer(){
+		String replyFromMasterServer;
+		
+		try {
+			socket = new Socket("localhost", listeningPortMasterServer);
+			din = new DataInputStream(socket.getInputStream());
+			dout = new DataOutputStream(socket.getOutputStream());
+			
+			// send registration request
+			dout.writeUTF("Registeration request from " + listeningPort
+					+ " " + chunkServerName);
+			// receive response from master server
+			System.out.println(din.readUTF());
+			
+		} catch (IOException e) {
+			System.err.println("Error encountered while creating a "
+					+ "socket!!! " + e);
+		} finally {
+			try {
+				dout.close();
+				din.close();
+				socket.close();
+			} catch (IOException e) {
+				System.err.println("Error while closing connections"
+						+ " (socket,etc) " + e);
+			}
+			
+		}
+	}
+	
+// ===========================================================================>>>>>>>>
+	
+	/*
+	 * This method is used to get listeningPort
+	 */
+	public int getListeningPort(){
+		return listeningPort;
+	}
+	
+	/*
+	 * This method is used to get listeningPort of master server
+	 */																			
+																			// check if this method is needed 
+	public int getListeningPortMasterServer(){
+		return listeningPortMasterServer;
+	}
+	
+	/*
+	 * This method is used to get chunkserver name
+	 */
+	public String getChunkserverName(){
+		return chunkServerName;
+	}
+	
+// ===========================================================================>>>>>>>>
+
 	/*
 	 * This method is used to display lines on console 
 	 * for making the screen elegant 
 	 */
 	public void displayLines(){
 		System.out.println("=================================>>");
+	}
+	
+	/*
+	 * This method is used to display pluses on console 
+	 * for making the screen elegant 
+	 */
+	public void displayCross(){
+		System.out.println("X==X==X==X==X==X==X==X==X==X==X==X=");
 	}
 	
 }
--- src/com/sdfs/masterServer/MasterServer.java
+++ src/com/sdfs/masterServer/MasterServer.java
@@ -1,5 +1,11 @@
 package com.sdfs.masterServer;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -12,8 +18,45 @@ import java.util.Scanner;
  */
 public class MasterServer {
 
-	static int listeningPort;
+	private static int listeningPort;
+	private static int activeChunkserver;
+	private static int backupChunkserver;
+	private static String[] activeChunkserverArray;
+	private static String[] backupChunkserverArray;
 	Scanner sc;
+	boolean keepListening;
+	
+	ServerSocket serverSocket;
+	Socket clientSocket;												// can make this local						
+	HashMap<String, Integer> chunkserverMap;
+	
+	/*
+	 * Constructor of MasterServer
+	 */
+	public MasterServer(){
+		keepListening = true;
+		activeChunkserverArray = new String[3];
+		backupChunkserverArray = new String[3];
+		chunkserverMap = new HashMap<>();
+	}
+	
+	/*
+	 * This method initializes Master Server i.e. takes listening port
+	 */
+	public void initialise(){
+		displayLines();
+		System.out.println("Initialising master");
+		
+		// take listening port
+		do {
+			listeningPort = inputListeningPort();
+		} while (listeningPort == 0);
+		// listen for incoming connections
+		listenRequests();
+		
+		displayLines();
+		displayLines();
+	}
 	
 	/*
 	 * This method is used to take port number that the Master Server
@@ -33,19 +76,189 @@ public class MasterServer {
 	}
 	
 	/*
-	 * This method initializes Master Server i.e. takes listening port
+	 * This method is used to listen for incoming requests.
+	 * Chunkserver or client can send requests 
 	 */
-	public void initialise(){
-		displayLines();
-		System.out.println("Initialising master");
+	public void listenRequests(){
+		try {
+			serverSocket = new ServerSocket(listeningPort);
+		} catch (IOException e) {
+			System.err.println("Error encountered while opening "
+					+ "serverSocket!!! " + e);
+		}
 		
-		// take listening port
-		do {
-			listeningPort = inputListeningPort();
-		} while (listeningPort == 0);
-		System.out.println("Master Server will listen for connections"
-				+ " on port: " + listeningPort);
-		displayLines();
+		while(keepListening){
+			displayLines();
+			System.out.println("Master Server listening for connections"
+					+ " on port: " + listeningPort);					// remove displaying listeningPort
+			
+			try {
+				clientSocket = serverSocket.accept();
+				processRequest(clientSocket);							
+				
+			} catch (IOException e) {
+				System.err.println("Error encountered while accepting "
+						+ "client connection!!! " + e);
+			} finally {
+				try {
+					clientSocket.close();
+				} catch (IOException e) {
+					System.err.println("Error while closing socket "
+							+ "connection!!! " + e);
+				}
+			}
+			
+			// for testing purposes
+			displayCross();													// remove
+			displayActiveChunkserver();										// remove
+			displayBackupChunkserver();										// remove
+		}
+	}
+	
+	/*
+	 * This method is used to process requests received by the master server
+	 */
+	public void processRequest(Socket socket){
+		String receivedRequest;
+		DataInputStream din = null;
+		DataOutputStream dout = null;
+		
+		try {
+			din = new DataInputStream(socket.getInputStream());
+			dout = new DataOutputStream(socket.getOutputStream());
+			receivedRequest = din.readUTF();
+			
+			if(receivedRequest.contains("Registeration request"))
+				registrationRequest(receivedRequest, dout);
+
+			} catch (IOException e) {
+			System.err.println("Error encountered while creating "
+					+ "data i/p & o/p streams!!! " + e);
+		} finally {
+			try {
+				din.close();
+				dout.close();
+			} catch (IOException e) {
+				System.err.println("Error while data i/p & o/p streams"
+						+ "!!! " + e);
+			}
+			
+		}
+		
+	}
+	
+	/*
+	 * This method is used to process registration request
+	 * received by the master server.
+	 */
+	public void registrationRequest(String request, 
+			DataOutputStream dout){
+		int chunkserverListeningPort = 0;
+		
+		String[] array = request.split(" ");
+		System.out.println("Registration request received from"
+				+ " " + array[array.length - 1]);
+		
+		try{
+			chunkserverListeningPort = Integer.parseInt(
+					array[array.length - 2]);
+		} catch(Exception e){
+			System.err.println("Error encountered while parsing"
+					+ " chunkserver listening port!!! " + e);
+		}
+		
+		
+		if(activeChunkserver <3)						// if active chunkserver no. is increased, then increase activeChunkserverArray
+			assignChunkserver(dout, array[array.length - 1], 
+					chunkserverListeningPort);
+		else if(backupChunkserver < 3)
+			assignBackupChunkserver(dout, array[array.length - 1], 
+					chunkserverListeningPort);
+		else{
+			try {
+				dout.writeUTF("No role assigned as full capacity "
+						+ "of chunkserver and backup chunkserver"
+						+ " has been reached");
+			} catch (IOException e) {
+				System.err.println("Error encountered while writing "
+						+ "in dout!!! " + e);
+			}
+		}
+			
+		/**
+		 * write a method to assign as backup chunkserver
+		 * 
+		 */
+		
+	}
+	
+	/*
+	 * This method is used to assign role of chunkserver
+	 */
+	public void assignChunkserver(DataOutputStream dout,
+			 String chunkserverName, int chunkserverListeningPort){
+		activeChunkserver++;
+		activeChunkserverArray[activeChunkserver - 1] = chunkserverName;
+		chunkserverMap.put(chunkserverName, 
+				chunkserverListeningPort);
+		
+		try {
+			dout.writeUTF("Your request has been received. You are"
+					+ " registered as Chunkserver.");
+		} catch (IOException e) {
+			System.err.println("Error encountered while writing"
+					+ " with data o/p!!! " + e);
+		}
+	}
+	
+	/*
+	 * This method is used to assign role of backup Chunkserver
+	 */
+	public void assignBackupChunkserver(DataOutputStream dout,
+			 String chunkserverName, int chunkserverListeningPort){
+		backupChunkserver++;
+		backupChunkserverArray[backupChunkserver - 1] = chunkserverName;
+		chunkserverMap.put(chunkserverName, 
+				chunkserverListeningPort);
+		
+		try {
+			dout.writeUTF("Your request has been received. You are"
+					+ " registered as Backup Chunkserver.");
+		} catch (IOException e) {
+			System.err.println("Error encountered while writing"
+					+ " with data o/p!!! " + e);
+		}
+	}
+	
+
+// ===========================================================================>>>>>>>>	
+	
+	/*
+	 * This method is used to see list of active chunkservers & their
+	 * listening ports
+	 */
+	public void displayActiveChunkserver(){
+		// display information of activeChunkserverArray
+		for(int i=0; i < activeChunkserver; i++){
+			System.out.println(activeChunkserverArray[i] + " "
+					+ chunkserverMap.get(activeChunkserverArray[i]));
+		}
+		
+	}
+	
+	/*
+	 * This method is used to see list of backup chunkservers & their
+	 * listening ports
+	 */
+	public void displayBackupChunkserver(){
+		if(backupChunkserver != 0)
+		
+		// display information of backupChunkserverArray
+		for(int i=0; i < backupChunkserver; i++){
+			System.out.println(backupChunkserverArray[i] + " "
+					+ chunkserverMap.get(backupChunkserverArray[i]));
+		}
+		
 	}
 	
 	/*
@@ -61,6 +274,14 @@ public class MasterServer {
 	 */
 	public void displayLines(){
 		System.out.println("=================================>>");
+	}
+	
+	/*
+	 * This method is used to display pluses on console 
+	 * for making the screen elegant 
+	 */
+	public void displayCross(){
+		System.out.println("X==X==X==X==X==X==X==X==X==X==X==X=");
 	}
 	
 }
